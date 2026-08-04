c6be2262dfc7d2902be7184401c11d94a5bc356a
Luka Jaklic <lukajaklic1@gmail.com>
2026-08-04 09:17:39 +0200

    Set primary colors: #266df0 blue, #101112 black

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,24 @@ export default {
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
       },
+      colors: {
+        blue: {
+          50:  '#eff5fe',
+          100: '#deeafd',
+          200: '#bed5fb',
+          300: '#8db8f8',
+          400: '#5a93f4',
+          500: '#3a7df2',
+          600: '#266df0',
+          700: '#1a5cd4',
+          800: '#154ab0',
+          900: '#123d8e',
+          950: '#0c2860',
+        },
+        gray: {
+          900: '#101112',
+        },
+      },
     },
   },
   plugins: [],
